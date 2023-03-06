0a699e0f8e459a26d2476469558e6bba63fbf321
Basavaprasad Mallikarjun Gola <basavaprasad.m.gola@gmail.com>
2023-03-05 23:48:48 -0600

    Strings and Images added

--- app/src/main/java/com/example/lemonadeb/MainActivity.kt
+++ app/src/main/java/com/example/lemonadeb/MainActivity.kt
@@ -3,41 +3,52 @@ package com.example.lemonadeb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonadeb.ui.theme.LemonadeBTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import androidx.compose.ui.Modifier as Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LemonadeBTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                LemonadeApp()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LemonadeApp(modifier: Modifier = Modifier
+    .fillMaxSize()
+) {
+    Text(text = "hello")
+//    LemonadeWithIamgeAndText()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    LemonadeBTheme {
-        Greeting("Android")
+fun LemonadeWithIamgeAndText(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+        Text(stringResource(id = R.string.tap_tree_directions))
+        Image(painter = painterResource(id = R.drawable.lemon_drink), contentDescription = result.toString() )
     }
 }
