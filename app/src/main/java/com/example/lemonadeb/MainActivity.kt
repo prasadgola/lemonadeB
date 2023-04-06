63715ed932816a4639193ee28b5a05761f98b696
Basavaprasad Mallikarjun Gola <basavaprasad.m.gola@gmail.com>
2023-04-06 04:12:10 -0500

    Final commit

--- app/src/main/java/com/example/lemonadeb/MainActivity.kt
+++ app/src/main/java/com/example/lemonadeb/MainActivity.kt
@@ -3,22 +3,28 @@ package com.example.lemonadeb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonadeb.ui.theme.LemonadeBTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import androidx.compose.foundation.layout.Column as Column
 import androidx.compose.ui.Modifier as Modifier
 
 class MainActivity : ComponentActivity() {
@@ -34,21 +40,115 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun LemonadeApp(modifier: Modifier = Modifier
-    .fillMaxSize()
+fun LemonadeApp() {
+
+    var currentStep by remember { mutableStateOf(1) }
+
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        when (currentStep) {
+            1 -> {
+                // Display lemon tree image and ask user to pick a lemon from the tree
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.tap_tree_directions,
+                    drawableResourceId = R.drawable.lemon_tree,
+                    contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                    onImageClick = {
+                        // Update to next step
+                        currentStep = 2
+                        // Each time a lemon is picked from the tree, get a new random number
+                        // between 2 and 4 (inclusive) for the number of times the lemon needs
+                        // to be squeezed to turn into lemonade
+                        squeezeCount = (2..4).random()
+                    }
+                )
+            }
+            2 -> {
+                // Display lemon image and ask user to squeeze the lemon
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.keep_tapping_directions,
+                    drawableResourceId = R.drawable.lemon_squeeze,
+                    contentDescriptionResourceId = R.string.lemon_content_description,
+                    onImageClick = {
+                        // Decrease the squeeze count by 1 for each click the user performs
+                        squeezeCount--
+                        // When we're done squeezing the lemon, move to the next step
+                        if (squeezeCount == 0) {
+                            currentStep = 3
+                        }
+                    }
+                )
+            }
+            3 -> {
+                // Display glass of lemonade image and ask user to drink the lemonade
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.drink_directions,
+                    drawableResourceId = R.drawable.lemon_drink,
+                    contentDescriptionResourceId = R.string.drink_directions,
+                    onImageClick = {
+                        // Update to next step
+                        currentStep = 4
+                    }
+                )
+            }
+            4 -> {
+                // Display empty glass image and ask user to start again
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.empty_glass_content_description,
+                    drawableResourceId = R.drawable.lemon_restart,
+                    contentDescriptionResourceId = R.string.empty_glass_content_description,
+                    onImageClick = {
+                        // Back to starting step
+                        currentStep = 1
+                    }
+                )
+            }
+        }
+    }
+}
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    LemonadeWithIamgeAndText()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(textLabelResourceId),
+            fontSize = 18.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(drawableResourceId),
+            contentDescription = stringResource(contentDescriptionResourceId),
+            modifier = Modifier
+                .wrapContentSize()
+                .clickable(
+                    onClick = onImageClick
+                )
+                .border(
+                    BorderStroke(2.dp, Color(105, 205, 216)),
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .padding(16.dp)
+        )
+    }
 }
 
+@Preview
 @Composable
-fun LemonadeWithIamgeAndText(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf(1) }
-    Column (
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-        Text(stringResource(id = R.string.tap_tree_directions))
-
-        Image(painter = painterResource(id = R.drawable.lemon_drink), contentDescription = result.toString() )
+fun LemonPreview() {
+    LemonadeBTheme() {
+        LemonadeApp()
     }
 }
